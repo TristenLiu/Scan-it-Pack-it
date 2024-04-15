7455606870c55119939d07a5f4419709cc9e871c
TristenLiu <98703889+TristenLiu@users.noreply.github.com>
2024-04-14 22:57:55 -0400

    Removed extra spaces

--- ScanitPackit/ScanitPackit/UsageViewModel.swift
+++ ScanitPackit/ScanitPackit/UsageViewModel.swift
@@ -16,7 +16,7 @@ struct UsageSection {
 class UsageViewModel: ObservableObject {
     @Published var sections: [UsageSection] = [
         UsageSection(title: "Scanning", 
-                     instructions: ["1) Press the (+) button on one point of the length of the                   box select the starting point",
+                     instructions: ["1) Press the (+) button on one point of the length of the box select the starting point",
                                     "2) Press the (+) on the other endpoint on the same length of the box to finish the edge measurement",
                                     "3) Repeat steps 1&2 two more times on the width and the height of the box",
                                     "4) To finish, return to the Manual Input screen",
@@ -27,7 +27,7 @@ class UsageViewModel: ObservableObject {
                                     "Toggle the Slider in order to register Container or Box dimensions",
                                     "Keep track of box input order for the Packing Schematic"]),
         UsageSection(title: "Manual Input",
-                     instructions: ["1) Press Add Container or Add Box to add a new field to the                 List",
+                     instructions: ["1) Press Add Container or Add Box to add a new field to the List",
                                     "2) Enter the dimensions of the Container/Box in the respective fields",
                                     "3) When finished entering all dimensions, Press the 'Finish Adding' button to move to the packing schematic",
                                     "---Notes---",
@@ -35,7 +35,7 @@ class UsageViewModel: ObservableObject {
                                     "Delete Containers/Boxes by pressing the delete button on the top right of the input field",
                                     "Keep track of box input order for the Packing Schematic"]),
         UsageSection(title: "Packing Schematic",
-                     instructions: ["1) On opening, set up the container according to the                        schematic container format",
+                     instructions: ["1) On opening, set up the container according to the schematic container format",
                                     "2) Press the right and left arrows to view the different packing order of the input boxes",
                                     "3) Pack the boxes according to the Container number and the Box number on the screen",
                                     "4) Toggle between Multiple Containers using the bottom right and left arrows",
